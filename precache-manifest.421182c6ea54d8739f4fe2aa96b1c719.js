43c1dcec3204cabff7156a92384427b0ce9d8d14
Abrar Ranjha <47588004+AbrarRanjh@users.noreply.github.com>
2021-09-19 12:30:54 +0500

    Updates

---
+++ precache-manifest.421182c6ea54d8739f4fe2aa96b1c719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bfbb7a8c564a4823ffea6b603bae8e",
+    "url": "/CafayList/index.html"
+  },
+  {
+    "revision": "3d20ab34f1852cf8b244",
+    "url": "/CafayList/static/css/2.07120c43.chunk.css"
+  },
+  {
+    "revision": "2929764c492d55a07f09",
+    "url": "/CafayList/static/css/main.6777a35d.chunk.css"
+  },
+  {
+    "revision": "3d20ab34f1852cf8b244",
+    "url": "/CafayList/static/js/2.b14eb417.chunk.js"
+  },
+  {
+    "revision": "cc8146eb981cd5dea75292a3ffa62913",
+    "url": "/CafayList/static/js/2.b14eb417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2929764c492d55a07f09",
+    "url": "/CafayList/static/js/main.79ca3f3f.chunk.js"
+  },
+  {
+    "revision": "ce0f612014cb5885921e",
+    "url": "/CafayList/static/js/runtime-main.a02ba46e.js"
+  }
+]);
